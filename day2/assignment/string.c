19300750172606d753599691b7ab2e535c5ddfb1
chaithranjali22 <chaithranjaliathavarathavar@gmail.com>
2025-02-18 14:51:30 +0530

    Create string.c

---
+++ day2/assignment/string.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+void reverseString(char *str) 
+{
+    char *end = str;
+    char temp;
+  
+    while (*end) 
+    {
+        end++;
+    }
+    end--; 
+   
+    while (str < end) 
+    {
+        temp = *str;
+        *str = *end;
+        *end = temp;
+        str++;
+        end--;
+    }
+}
+
+int main() {
+    char str[100];
+    printf("Enter a string: ");
+    scanf("%s", str);
+    reverseString(str);
+    printf("Reversed String: %s\n", str);
+
+    return 0;
+}
